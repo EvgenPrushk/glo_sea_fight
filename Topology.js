9576fd6544faa1589c940b2176abf979dcb91f15
Прушак Евгений Владимирович <D:\poshta>
2020-03-12 19:39:56 +0300

    feat: add draw sheep + check function

--- Topology.js
+++ Topology.js
@@ -2,10 +2,10 @@
 //     name: 'Alex',
 //     surname: 'Darmax',
 //     age: 27,
-    
+
 // greeting() {
 //     console.log(`Hello! I am ${this.name} ${this.surname}`);
-    
+
 // }
 // };
 
@@ -13,10 +13,10 @@
 //     name: 'Timo',
 //     surname: 'Darmax',
 //     age: 27,
-    
+
 // greeting() {
 //     console.log(`Hello! I am ${this.name} ${this.surname}`);
-    
+
 // }
 // };
 //фабрика
@@ -25,14 +25,14 @@
 //         name,
 //         surname,
 //         age,
-        
-    // greeting() {
-    //     console.log(`Hello! I am ${this.name} ${this.surname}`);
-        
+
+// greeting() {
+//     console.log(`Hello! I am ${this.name} ${this.surname}`);
+
 //     },
 //         wall    () {
 //             console.log('Go to home');
-            
+
 //         },
 // //     }
 // };
@@ -51,16 +51,123 @@
 
 //     wall    () {
 //         console.log('Go to home');
-        
+
 //     }
 
 // };
 // const person1 = new Person('Alex', 'Dark', 27);
 // const person2 = new Person('Maks', 'Solid', 37);
 
-class Topology{
-    constructor(){
+class Topology {
+    constructor(param) {
+        this.offsetX = param.offsetX;
+        this.offsetY = param.offsetY;
+
         this.sheeps = [];
         this.checks = [];
     }
-}
+
+    addSheeps (...sheeps) {
+        for (const sheep of sheeps) {
+            if (!this.sheeps.includes(sheep)) {
+                this.sheeps.push(sheep);
+            }
+        }
+        return this
+    }
+
+    addChecks (...checks) {
+        for (const check of checks) {
+            if (!this.checks.includes(check)) {
+                this.checks.push(check);
+            }
+        }
+        return this
+    }
+
+    draw(context){
+        for (const sheep of this.sheeps) {
+            this.drawSheep(context, sheep);
+        }
+        for (const check of this.checks) {
+            this.drawSheep(context, check);
+        }
+    }
+
+    drawFields(context) {
+        context.strokeStyle = 'blue';
+        context.lineWidth = 1.7;
+        for (let i = 1; i <= 11; i++) {
+            context.beginPath();
+            context.moveTo(
+                this.offsetX + i * FIELD_SIZE,
+                this.offsetY
+            );
+            context.lineTo(
+                this.offsetX + i * FIELD_SIZE,
+                this.offsetY + 11 * FIELD_SIZE
+            );
+            context.stroke();
+        }
+
+        for (let i = 1; i <= 11; i++) {
+            context.beginPath();
+            context.moveTo(
+                this.offsetX,
+                this.offsetY + i * FIELD_SIZE
+            );
+            context.lineTo(
+                this.offsetX + 11 * FIELD_SIZE,
+                this.offsetY + i * FIELD_SIZE
+            );
+            context.stroke();
+        }
+
+        context.textAlign = "center";
+        context.font = " 20px comic sans";
+        const alphabet ="АБВГДЕЖЗИК";
+        for (let i = 0; i < 10; i++) {
+           const lettter = alphabet[i];
+            context.fillText(
+                lettter,
+                this.offsetX + i * FIELD_SIZE + FIELD_SIZE * 1.5,
+                this.offsetY + FIELD_SIZE * 0.8
+                );
+        }
+        for (let i = 1; i <= 10; i++) {           
+             context.fillText(
+                 i,
+                 this.offsetX +  FIELD_SIZE * 0.5,                 
+                 this.offsetY + i * FIELD_SIZE + FIELD_SIZE * 0.8,
+                 );
+         }
+         return this
+    }
+    drawSheep(context, sheep){
+        context.fillStyle = "rgba(0, 0, 0, 0.75";
+
+        context.beginPath();
+        context.rect(
+            this.offsetX + sheep.x * FIELD_SIZE + FIELD_SIZE + 2,
+            this.offsetY + sheep.y * FIELD_SIZE + FIELD_SIZE + 2,
+            (sheep.direct === 0 ? sheep.size : 1) * FIELD_SIZE - 4,
+            (sheep.direct === 1 ? sheep.size : 1) * FIELD_SIZE - 4,
+        );
+        context.fill();
+
+    }
+    drawCheck(context, check){
+        context.fillStyle = "black";
+
+        context.beginPath();
+        context.arc(
+            this.offsetX + check.x * FIELD_SIZE + FIELD_SIZE * 1.5,
+            this.offsetY + check.y * FIELD_SIZE + FIELD_SIZE * 1.5,
+            3,
+            0,
+            Math.PI *2,
+        );
+        context.fill();
+    }
+    
+};
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const CELL_SIZE = 23;
+const FIELD_SIZE = 30;
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
-canvas.width  = 500;
+canvas.width = 500;
 canvas.height = 500;
 
 // drawRect({
@@ -19,40 +20,59 @@ canvas.height = 500;
 //     lineWidth: 2,
 
 // })
+
+const player = new Topology({
+    offsetX: 50,
+    offsetY: 87,
+});
+
+player.
+    addSheeps(
+        {x: 0, y: 0, direct: 0, size: 3},
+        {x: 0, y: 2, direct: 1, size: 4},
+    )
+    .addChecks(
+        {x: 5, y: 5},
+        {x: 5, y: 4},
+    )
+
+
 drawGrid();
+player.drawFields(context);
+player.draw(context);
 
 function drawRect(param) {
     if (!param.fill && !param.stroke) {
         return
     }
-  context.beginPath();
-  context.rect(param.x, param.y, param.width, param.height);
-  
-  if (param.fill) {
-        context.fillStyle = param.fillStyle;
-      context.fill();
-  }
+    context.beginPath();
+    context.rect(param.x, param.y, param.width, param.height);
 
-  if (param.stroke) {
-    context.strokeStyle = param.strokeStyle;  
-    context.lineWidth = param.lineWidth;
-    context.stroke();
+    if (param.fill) {
+        context.fillStyle = param.fillStyle;
+        context.fill();
+    }
+
+    if (param.stroke) {
+        context.strokeStyle = param.strokeStyle;
+        context.lineWidth = param.lineWidth;
+        context.stroke();
     }
 };
 
 function drawGrid() {
     context.strokeStyle = "blue";
     context.lineWidth = 0.5;
-    for (let i = 0; i < canvas.width / CELL_SIZE; i++){
+    for (let i = 0; i < canvas.width / CELL_SIZE; i++) {
         context.beginPath();
         context.moveTo(i * CELL_SIZE, 0);
         context.lineTo(i * CELL_SIZE, canvas.height);
         context.stroke();
     }
-    for (let i = 0; i < canvas.height / CELL_SIZE; i++){
+    for (let i = 0; i < canvas.height / CELL_SIZE; i++) {
         context.beginPath();
         context.moveTo(0, i * CELL_SIZE);
-        context.lineTo(canvas.width,  i * CELL_SIZE);
+        context.lineTo(canvas.width, i * CELL_SIZE);
         context.stroke();
     }
     context.lineWidth = 2;
